84093a65ce652c9a960f4d99d02ce6a751d1b9ae
Colin Beuttler <44789660+ColinBeuttler@users.noreply.github.com>
2023-03-27 14:11:44 -0700

    Update MainActivity.kt

--- app/src/main/java/com/colinbeuttler/logintemplate/MainActivity.kt
+++ app/src/main/java/com/colinbeuttler/logintemplate/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.colinbeuttler.logintemplate
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.textfield.TextInputEditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        register_button.setOnClickListener { previewLogIn() }
+        var newUserInfo = createPreview()
+        var registerButton = findViewById<Button>(R.id.register_button)
+        registerButton.setOnClickListener { previewLogIn(newUserInfo) }
     }
 
-    private fun previewLogIn() {
-        println("made text")
+    private fun createPreview(): LogInInfo {
+        var firstName = findViewById<TextInputEditText>(R.id.text_input_first_name)
+        var lastName = findViewById<TextInputEditText>(R.id.text_input_last_name)
+        var emailInput = findViewById<TextInputEditText>(R.id.text_input_email)
+        var inputNumber = findViewById<TextInputEditText>(R.id.text_input_phone_number)
+        var passwordInput = findViewById<TextInputEditText>(R.id.text_input_password)
+        return LogInInfo(firstName, lastName, emailInput, inputNumber, passwordInput)
+    }
+    private fun previewLogIn(userInfo: LogInInfo) {
+        println(userInfo)
+        Toast.makeText(this,userInfo.toString(), Toast.LENGTH_LONG).show()
     }
 }
